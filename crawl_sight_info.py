b8fdbe718d448d80d9fd6e22fb99d84cad9d9afa
Wangorz <782562661@qq.com>
2019-11-10 13:36:03 +0800

    lpush start_urls

--- crawl_sight_info.py
+++ crawl_sight_info.py
@@ -3,9 +3,5 @@
 from scrapy import cmdline
 # cmdline.execute('scrapy crawl sight'.split())
 # cmdline.execute('scrapy crawl qunar'.split())
-import redis
-redis_db = redis.Redis(host='47.97.198.174', port=6379, db=0)
-redis_db.lpush('qunar:start_urls', 'https://piao.qunar.com/ticket/list.htm?keyword=上海&region=上海&from=mpshouye_hotcity')
 cmdline.execute('scrapy crawl sight_info'.split())
-
 
--- crawl_sight_url.py
+++ crawl_sight_url.py
@@ -2,5 +2,12 @@
 
 from scrapy import cmdline
 # cmdline.execute('scrapy crawl sight'.split())
+
+import redis
+redis_db = redis.Redis(host='47.97.198.174', port=6379, db=0)
+redis_db.lpush('qunar:start_urls', 'https://piao.qunar.com/ticket/list.htm?keyword=上海&region=上海&from=mpshouye_hotcity')
 cmdline.execute('scrapy crawl qunar'.split())
+
 # cmdline.execute('scrapy crawl sight_info'.split())
+
+
