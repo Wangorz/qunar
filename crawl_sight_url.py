281218d04034fb284c15bd2bebdd0c033360637b
Wangorz <782562661@qq.com>
2019-11-10 13:38:25 +0800

    lpush start_urls

--- crawl_sight_url.py
+++ crawl_sight_url.py
@@ -5,6 +5,3 @@ import redis
 redis_db = redis.Redis(host='47.97.198.174', port=6379, db=0)
 redis_db.lpush('qunar:start_urls', 'https://piao.qunar.com/ticket/list.htm?keyword=上海&region=上海&from=mpshouye_hotcity')
 cmdline.execute('scrapy crawl qunar'.split())
-
-
-
